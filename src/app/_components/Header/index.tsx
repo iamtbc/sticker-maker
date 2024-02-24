3141233f8aea9ad2c9ea73bd7956ee3fe0d39641
iamtbc <kosuke.tabuchi@mirumill.com>
2024-02-24 14:25:40 +0900

    feat: add a link to the top page on the header

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -2,8 +2,12 @@ const Header = () => {
   return (
     <>
       <header className="py-6 md:px-8 border-b">
-        <div className="container flex flex-col items-center justify-between md:flex-row">
-          <h2 className="text-lg font-semibold">StickerMaker</h2>
+        <div className="container flex flex-col md:flex-row items-center justify-between">
+          <div className="flex">
+            <a href="/">
+              <h2 className="text-lg font-semibold">StickerMaker</h2>
+            </a>
+          </div>
         </div>
       </header>
     </>
